18e7d8235033feaa4765beb87c92e474aa392680
NaghiyevDev <rashadnagiyev1900@gmail.com>
2024-12-11 18:03:48 +0300

    Update 11.12.2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@ import { getBlogs, getLatestBlogs } from './fetchBlogList.js';
 import { getProjects } from './fetchMyProjects.js';
 
   
+
+
+
 const pageName = window.location.pathname.split('/').pop();
 console.log(pageName);
 if (pageName === 'index.html' || pageName === '/' || pageName === '') {
@@ -11,5 +14,29 @@ if (pageName === 'index.html' || pageName === '/' || pageName === '') {
 };
 if (pageName === 'blogs.html' || pageName === '/blogs' || pageName === 'blogs') {
     getBlogs();
-};
+    
+    function togglePaginationView() {
+      const isMobile = window.innerWidth <= 768;
+      
+      // Select pagination sections
+      const responsiveNav = document.querySelector('.responsive-pagination');
+      const desktopNav = document.querySelector('.desktop-pagination');
 
+      // Dynamically set IDs
+      if (isMobile) {
+          responsiveNav.id = 'pagination-container';
+          desktopNav.removeAttribute('id');
+      } else {
+          desktopNav.id = 'pagination-container';
+          responsiveNav.removeAttribute('id');
+      }
+
+      // Toggle display
+      document.querySelector('.pagination-section.responsive').style.display = isMobile ? 'block' : 'none';
+      document.querySelector('.pagination-section.desktop').style.display = isMobile ? 'none' : 'block';
+    }
+
+    // Initial check and event listener
+    window.addEventListener('resize', togglePaginationView);
+    togglePaginationView();
+};
